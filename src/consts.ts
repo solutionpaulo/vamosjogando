e66f7668982828cfe64b1b0a92503d6afc727ef6
solutionpaulo <solution.paulo@gmail.com>
2026-05-21 13:13:35 -0300

    Adiciona SITE_BASE em todos os links do projeto

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Vamos Jogando';
 export const SITE_DESCRIPTION = 'Seu portal de notícias de jogos inteligente e automatizado!';
+export const SITE_BASE = '/vamosjogando';
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -14,7 +14,7 @@ export async function GET(context) {
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/blog/${post.id}/`,
+      link: new URL(`/blog/${post.id}/`, context.site).pathname,
     })),
   });
 }
